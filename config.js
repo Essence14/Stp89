8032bae9cf937d4bfefaa397eaf68cd32a9eac86
Essence14 <91456971+Essence14@users.noreply.github.com>
2022-04-03 18:02:22 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIoA1sL1VAKBHtjG9-Gu4aurmHGRGyCW4",
+  authDomain: "stp84-3b386.firebaseapp.com",
+  databaseURL: "https://stp84-3b386-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stp84-3b386",
+  storageBucket: "stp84-3b386.appspot.com",
+  messagingSenderId: "100339655884",
+  appId: "1:100339655884:web:cca451b61f422c6a1cce69"
+};
